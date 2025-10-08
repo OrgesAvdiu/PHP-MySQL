1ec2b5c865c9f37052a8c8b0f97a94b54b8fa233
unknown <Student@digitalschool.local>
2025-10-08 20:38:18 +0200

    multidimensional & associative arrays and nested loops

---
+++ Module_5/AssociativeArrays.php
@@ -0,0 +1,15 @@
+<?php
+
+    $grade = array(
+        "Math" => "3",
+        "Art" => "5",
+        "History" => "4"
+    );
+    echo "Math Grade is " . $grade['Math'];
+    echo "<br>";
+
+    foreach($grade as $subject => $grade){
+        echo "Subject: " . $subject . ", Grade :" . $grade;
+        echo "<br>";
+    }
+?>
