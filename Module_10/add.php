82b28f61a1c6f7bfd22348e6728fe4cbeb93484e
unknown <Student@DIGITALSCHOOL.LOCAL>
2025-11-05 20:47:25 +0100

    Add dashboard

--- Module_10/add.php
+++ Module_10/add.php
@@ -27,6 +27,7 @@
 <html>
     <head></head>
     <body>
+        <a href="dashboard.php">Dashboard</a>
         <form action="add.php" method="POST">
             <input type="text" name="name" placeholder="Name"></br>
             <input type="text" name="surname" placeholder="Surname"></br>
